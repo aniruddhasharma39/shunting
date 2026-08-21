ea6aac292abb7d543efd7e16dddf3f3f29360cbf
Aastha0625 <justofficial06@gmail.com>
2026-08-22 01:54:35 +0530

    Fix 2

---
+++ backend/offline.js
@@ -0,0 +1,3 @@
+﻿require('dotenv').config();
+const db = require('./config/db');
+db.query("UPDATE devices SET network_status = 'Offline' WHERE id != 'ee7d38dc-615a-4b4f-8b84-15cf158396eb'").then(() => { console.log('Done'); process.exit(0); });
